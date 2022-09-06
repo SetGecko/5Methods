179f77f2ba67d3a23e6bfdcc0d2f38646a0b41ba
Алексей Ипполитов <setgecko@gmail.com>
2022-09-06 23:54:35 +0300

    Прошёл практику по 5.0 - повторение 4 модуля, создал проект 5.1 Методы

---
+++ 5.0Remember4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._0Remember4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] dishes) User;
+            Console.WriteLine("Введите имя пользователя");
+            User.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите 5 любибых блюд");
+            User.dishes = new string[5];
+            for (int i = 0; i < User.dishes.Length ;i++)
+            {
+                User.dishes[i] = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
