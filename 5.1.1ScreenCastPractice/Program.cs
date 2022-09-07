9122fa3330d73a137e8ea43964f742090ced521e
Алексей Ипполитов <setgecko@gmail.com>
2022-09-07 13:57:37 +0300

    Выполнена практика по модулю 5.1

---
+++ 5.1.1ScreenCastPractice/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1._1ScreenCastPractice
+{
+    internal class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+            for (int i = 0; i < result.Length; i ++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+                for (int j = i + 1;j < result.Length; j++)
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+
+            //var array = new int[5];
+            //var array = new int[5];
+            int[] array = GetArrayFromConsole();
+            //Console.WriteLine(array);
+
+            //GetArrayFromConsole();
+            Console.ReadKey();
+        }
+    }
+}
--- 5.1Methods/Program.cs
+++ 5.1Methods/Program.cs
@@ -8,7 +8,7 @@ namespace _5._1Methods
 {
     internal class Program
     {
-        static void ShowColor()
+        static string ShowColor()
         {
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
             var color = Console.ReadLine();
@@ -40,11 +40,24 @@ namespace _5._1Methods
 
                     Console.WriteLine("Your color is yellow!");
                     break;
+                
             }
+            return color;
         }
         static void Main(string[] args)
         {
-            ShowColor();
+            var favcolors = new string[3];
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+
+            Console.WriteLine("Ваши любимые цвета:");
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+
             Console.ReadKey();
         }
     }
