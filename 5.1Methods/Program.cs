692fc60e31cc3886a27213281cc2ec5a15bacf97
Алексей Ипполитов <setgecko@gmail.com>
2022-09-07 12:24:16 +0300

    Сделал метод ShowColor, который вызывается в основном теле программы

--- 5.1Methods/Program.cs
+++ 5.1Methods/Program.cs
@@ -8,9 +8,44 @@ namespace _5._1Methods
 {
     internal class Program
     {
+        static void ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-
+            ShowColor();
+            Console.ReadKey();
         }
     }
 }
